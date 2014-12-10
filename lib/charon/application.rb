f4fb5c36e147b0b36771b97807dba3f5c5f24145
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-12-10 15:32:24 -0800

    trapping signals happens IN the thing that uses EM

--- lib/charon/application.rb
+++ lib/charon/application.rb
@@ -13,9 +13,22 @@ module Charon
       pipe = IO.new(fd, Fcntl::O_RDONLY|Fcntl::O_NONBLOCK)
 
       EventMachine.run do
+        trap_signals
         listener = EM.watch(pipe, Charon::Listener)
         listener.notify_readable = true
       end
+    end
+
+    private
+
+    def trap_signals
+      trap('INT') {
+        EventMachine.stop_event_loop
+      }
+
+      trap('TERM') {
+        EventMachine.stop_event_loop
+      }
     end
   end
 end
--- lib/commands/start.rb
+++ lib/commands/start.rb
@@ -5,7 +5,6 @@ class CharonApp < Thor
   desc 'Start the Charon daemon', usage: 'charon start [config.yml]'
 
   def start(config_path = DEFAULT_CONFIG)
-    trap_signals
     Charon::Settings.source(config_path)
     require 'charon/application'
     Charon::Application.new.start
@@ -14,14 +13,4 @@ class CharonApp < Thor
   private
 
   DEFAULT_CONFIG = File.expand_path('../../../config/charon.yml', __FILE__)
-
-  def trap_signals
-    trap('INT') {
-      EventMachine.stop_event_loop
-    }
-
-    trap('TERM') {
-      EventMachine.stop_event_loop
-    }
-  end
 end
